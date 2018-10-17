bc73b4c9b43e25f666d35a49cb4c055743632b55
Team 5 <zhsh@umich.edu>
2018-10-17 11:55:28 -0400

    Format main

--- test_motors/test_motors.c
+++ test_motors/test_motors.c
@@ -22,68 +22,70 @@
 * int main() 
 *
 *******************************************************************************/
-int main(){
+int main() {
 
-	// make sure another instance isn't running
+    // make sure another instance isn't running
     // if return value is -3 then a background process is running with
     // higher privaledges and we couldn't kill it, in which case we should
     // not continue or there may be hardware conflicts. If it returned -4
     // then there was an invalid argument that needs to be fixed.
-    if(rc_kill_existing_process(2.0)<-2) return -1;
-
-	// start signal handler so we can exit cleanly
-    if(rc_enable_signal_handler()==-1){
-                fprintf(stderr,"ERROR: failed to start signal handler\n");
-                return -1;
+    if (rc_kill_existing_process(2.0) < -2) {
+        return -1;
     }
 
-    if(mb_motor_init()<0){
-        fprintf(stderr,"ERROR: failed to initialze mb_motors\n");
+    // start signal handler so we can exit cleanly
+    if (rc_enable_signal_handler() == -1) {
+        fprintf(stderr, "ERROR: failed to start signal handler\n");
+        return -1;
+    }
+
+    if (mb_motor_init() < 0) {
+        fprintf(stderr, "ERROR: failed to initialze mb_motors\n");
         return -1;
     }
 
     // make PID file to indicate your project is running
-	// due to the check made on the call to rc_kill_existing_process() above
-	// we can be fairly confident there is no PID file already and we can
-	// make our own safely.
-	rc_make_pid_file();
+    // due to the check made on the call to rc_kill_existing_process() above
+    // we can be fairly confident there is no PID file already and we can
+    // make our own safely.
+    rc_make_pid_file();
 
-	// done initializing so set state to RUNNING
-	rc_set_state(RUNNING); 
-	// Keep looping until state changes to EXITING
-	while(rc_get_state()!=EXITING){
-		// handle other states
-		if(rc_get_state()==RUNNING){
-			mb_motor_brake(1);
-			//run right forward for 1s
-			mb_motor_set(RIGHT_MOTOR, 0.8);
-			mb_motor_set(LEFT_MOTOR, 0.0);
-			rc_nanosleep(1E9);
-			//run left forward for 1s
-			mb_motor_set(RIGHT_MOTOR, 0.0);
-			mb_motor_set(LEFT_MOTOR, 0.8);
-			rc_nanosleep(1E9);
-			//run left backwards for 1s
-			mb_motor_set(RIGHT_MOTOR, 0.0);
-			mb_motor_set(LEFT_MOTOR, -0.8);
-			rc_nanosleep(1E9);
-			//run right backwards for 1s
-			mb_motor_set(RIGHT_MOTOR, -0.8);
-			mb_motor_set(LEFT_MOTOR, 0.0);
-			rc_nanosleep(1E9);
-			//set both forwards for 1s
-			mb_motor_brake(0);
-			mb_motor_set_all(0.8);
-			rc_nanosleep(1E9);
-			//stop motors for 1s
-			mb_motor_disable();
-			rc_nanosleep(2E9);
-		}
-		rc_nanosleep(1E9);
-	}
-	
-	// exit cleanly
-	mb_motor_cleanup();
-	rc_remove_pid_file();   // remove pid file LAST
-	return 0;
+    // done initializing so set state to RUNNING
+    rc_set_state(RUNNING);
+    // Keep looping until state changes to EXITING
+    while (rc_get_state() != EXITING) {
+        // handle other states
+        if (rc_get_state() == RUNNING) {
+            mb_motor_brake(1);
+            //run right forward for 1s
+            mb_motor_set(RIGHT_MOTOR, 0.8);
+            mb_motor_set(LEFT_MOTOR, 0.0);
+            rc_nanosleep(1E9);
+            //run left forward for 1s
+            mb_motor_set(RIGHT_MOTOR, 0.0);
+            mb_motor_set(LEFT_MOTOR, 0.8);
+            rc_nanosleep(1E9);
+            //run left backwards for 1s
+            mb_motor_set(RIGHT_MOTOR, 0.0);
+            mb_motor_set(LEFT_MOTOR, -0.8);
+            rc_nanosleep(1E9);
+            //run right backwards for 1s
+            mb_motor_set(RIGHT_MOTOR, -0.8);
+            mb_motor_set(LEFT_MOTOR, 0.0);
+            rc_nanosleep(1E9);
+            //set both forwards for 1s
+            mb_motor_brake(0);
+            mb_motor_set_all(0.8);
+            rc_nanosleep(1E9);
+            //stop motors for 1s
+            mb_motor_disable();
+            rc_nanosleep(2E9);
+        }
+        rc_nanosleep(1E9);
+    }
+
+    // exit cleanly
+    mb_motor_cleanup();
+    rc_remove_pid_file();   // remove pid file LAST
+    return 0;
 }
