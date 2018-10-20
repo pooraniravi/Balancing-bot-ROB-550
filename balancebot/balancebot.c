e518dee9d0216165e90d2fa28131241bb9fba005
Team 5 <zhsh@umich.edu>
2018-10-20 18:12:36 -0400

    Report initialization failures

--- balancebot/balancebot.c
+++ balancebot/balancebot.c
@@ -25,43 +25,50 @@
 * int main() 
 *
 *******************************************************************************/
-int main(){
+int main()
+{
 	// make sure another instance isn't running
-    // if return value is -3 then a background process is running with
-    // higher privaledges and we couldn't kill it, in which case we should
-    // not continue or there may be hardware conflicts. If it returned -4
-    // then there was an invalid argument that needs to be fixed.
-    if(rc_kill_existing_process(2.0)<-2) return -1;
+	// if return value is -3 then a background process is running with
+	// higher privaledges and we couldn't kill it, in which case we should
+	// not continue or there may be hardware conflicts. If it returned -4
+	// then there was an invalid argument that needs to be fixed.
+	if (rc_kill_existing_process(2.0) < -2)
+		return -1;
 
 	// start signal handler so we can exit cleanly
-    if(rc_enable_signal_handler()==-1){
-        fprintf(stderr,"ERROR: failed to start signal handler\n");
-        return -1;
-    }
-
-	if(rc_cpu_set_governor(RC_GOV_PERFORMANCE)<0){
-        fprintf(stderr,"Failed to set governor to PERFORMANCE\n");
-        return -1;
-    }
-
-	// initialize enocders
-    if(rc_encoder_eqep_init()==-1){
-        fprintf(stderr,"ERROR: failed to initialize eqep encoders\n");
-        return -1;
-    }
-
-    // initialize adc
-    if(rc_adc_init()==-1){
-        fprintf(stderr, "ERROR: failed to initialize adc\n");
-        return -1;
-    }
-
-    if(rc_dsm_init()==-1){
-		fprintf(stderr,"failed to start initialize DSM\n");
+	if (rc_enable_signal_handler() == -1)
+	{
+		fprintf(stderr, "ERROR: failed to start signal handler\n");
 		return -1;
 	}
 
-    // make PID file to indicate your project is running
+	if (rc_cpu_set_governor(RC_GOV_PERFORMANCE) < 0)
+	{
+		fprintf(stderr, "Failed to set governor to PERFORMANCE\n");
+		return -1;
+	}
+
+	// initialize enocders
+	if (rc_encoder_eqep_init() == -1)
+	{
+		fprintf(stderr, "ERROR: failed to initialize eqep encoders\n");
+		return -1;
+	}
+
+	// initialize adc
+	if (rc_adc_init() == -1)
+	{
+		fprintf(stderr, "ERROR: failed to initialize adc\n");
+		return -1;
+	}
+
+	if (rc_dsm_init() == -1)
+	{
+		fprintf(stderr, "failed to start initialize DSM\n");
+		return -1;
+	}
+
+	// make PID file to indicate your project is running
 	// due to the check made on the call to rc_kill_existing_process() above
 	// we can be fairly confident there is no PID file already and we can
 	// make our own safely.
@@ -70,14 +77,13 @@ int main(){
 	// start printf_thread if running from a terminal
 	// if it was started as a background process then don't bother
 	printf("starting print thread... \n");
-	pthread_t  printf_thread;
-	rc_pthread_create(&printf_thread, printf_loop, (void*) NULL, SCHED_OTHER, 0);
+	pthread_t printf_thread;
+	rc_pthread_create(&printf_thread, printf_loop, (void *)NULL, SCHED_OTHER, 0);
 
 	// start control thread
 	printf("starting setpoint thread... \n");
-	pthread_t  setpoint_control_thread;
-	rc_pthread_create(&setpoint_control_thread, setpoint_control_loop, (void*) NULL, SCHED_FIFO, 50);
-
+	pthread_t setpoint_control_thread;
+	rc_pthread_create(&setpoint_control_thread, setpoint_control_loop, (void *)NULL, SCHED_FIFO, 50);
 
 	// TODO: start motion capture message recieve thread
 
@@ -89,7 +95,8 @@ int main(){
 	mpu_config.orient = ORIENTATION_Z_UP;
 
 	// now set up the imu for dmp interrupt operation
-	if(rc_mpu_initialize_dmp(&mpu_data, mpu_config)){
+	if (rc_mpu_initialize_dmp(&mpu_data, mpu_config))
+	{
 		printf("rc_mpu_initialize_failed\n");
 		return -1;
 	}
@@ -97,32 +104,43 @@ int main(){
 	//rc_nanosleep(5E9); // wait for imu to stabilize
 
 	//initialize state mutex
-    pthread_mutex_init(&state_mutex, NULL);
-    pthread_mutex_init(&setpoint_mutex, NULL);
+	pthread_mutex_init(&state_mutex, NULL);
+	pthread_mutex_init(&setpoint_mutex, NULL);
 
 	//attach controller function to IMU interrupt
 	printf("initializing controller...\n");
-	mb_controller_init();
+	if (mb_controller_init() == -1)
+	{
+		printf("controller initialization failed\n");
+		return -1;
+	}
 
 	printf("initializing motors...\n");
-	mb_motor_init();
+	if (mb_motor_init() == -1)
+	{
+		printf("motor initialization failed\n");
+		return -1;
+	}
 
 	printf("resetting encoders...\n");
 	rc_encoder_eqep_write(1, 0);
 	rc_encoder_eqep_write(2, 0);
 
 	printf("initializing odometry...\n");
-	mb_odometry_init(&mb_odometry, 0.0,0.0,0.0);
+	mb_odometry_init(&mb_odometry, 0.0, 0.0, 0.0);
 
 	printf("attaching imu interupt...\n");
 	rc_mpu_set_dmp_callback(&balancebot_controller);
 
 	printf("we are running!!!...\n");
 	// done initializing so set state to RUNNING
-	rc_set_state(RUNNING); 
+	rc_set_state(RUNNING);
 
 	// Keep looping until state changes to EXITING
-	while(rc_get_state()!=EXITING){
+	while (rc_get_state() != EXITING)
+	{
+
+		// TODO have a calibration routine that we can comment out
 
 		// all the balancing is handled in the imu interupt function
 		// other functions are handled in other threads
@@ -130,16 +148,15 @@ int main(){
 		// always sleep at some point
 		rc_nanosleep(1E9);
 	}
-	
+
 	// exit cleanly
 	rc_mpu_power_off();
 	mb_motor_cleanup();
 	rc_led_cleanup();
 	rc_encoder_eqep_cleanup();
-	rc_remove_pid_file(); // remove pid file LAST 
+	rc_remove_pid_file(); // remove pid file LAST
 	return 0;
 }
-
 
 /*******************************************************************************
 * void balancebot_controller()
@@ -151,7 +168,8 @@ int main(){
 * 
 *
 *******************************************************************************/
-void balancebot_controller(){
+void balancebot_controller()
+{
 
 	//lock state mutex
 	pthread_mutex_lock(&state_mutex);
@@ -160,24 +178,23 @@ void balancebot_controller(){
 	// Read encoders
 	mb_state.left_encoder = rc_encoder_eqep_read(1);
 	mb_state.right_encoder = rc_encoder_eqep_read(2);
-    // Update odometry 
- 
+	// Update odometry
 
-    // Calculate controller outputs
-    
-    if(!mb_setpoints.manual_ctl){
-    	//send motor commands
-   	}
+	// Calculate controller outputs
 
-    if(mb_setpoints.manual_ctl){
-    	//send motor commands
-   	}
+	if (!mb_setpoints.manual_ctl)
+	{
+		//send motor commands
+	}
 
-   	//unlock state mutex
-    pthread_mutex_unlock(&state_mutex);
+	if (mb_setpoints.manual_ctl)
+	{
+		//send motor commands
+	}
 
+	//unlock state mutex
+	pthread_mutex_unlock(&state_mutex);
 }
-
 
 /*******************************************************************************
 *  setpoint_control_loop()
@@ -186,22 +203,22 @@ void balancebot_controller(){
 *
 *
 *******************************************************************************/
-void* setpoint_control_loop(void* ptr){
+void *setpoint_control_loop(void *ptr)
+{
 
-	while(1){
+	while (1)
+	{
 
-		if(rc_dsm_is_new_data()){
-				// TODO: Handle the DSM data from the Spektrum radio reciever
-				// You may should implement switching between manual and autonomous mode
-				// using channel 5 of the DSM data.
+		if (rc_dsm_is_new_data())
+		{
+			// TODO: Handle the DSM data from the Spektrum radio reciever
+			// You may should implement switching between manual and autonomous mode
+			// using channel 5 of the DSM data.
 		}
-	 	rc_nanosleep(1E9 / RC_CTL_HZ);
+		rc_nanosleep(1E9 / RC_CTL_HZ);
 	}
 	return NULL;
 }
-
-
-
 
 /*******************************************************************************
 * printf_loop() 
@@ -211,12 +228,15 @@ void* setpoint_control_loop(void* ptr){
 *
 * TODO: Add other data to help you tune/debug your code
 *******************************************************************************/
-void* printf_loop(void* ptr){
+void *printf_loop(void *ptr)
+{
 	rc_state_t last_state, new_state; // keep track of last state
-	while(rc_get_state()!=EXITING){
+	while (rc_get_state() != EXITING)
+	{
 		new_state = rc_get_state();
 		// check if this is the first time since being paused
-		if(new_state==RUNNING && last_state!=RUNNING){
+		if (new_state == RUNNING && last_state != RUNNING)
+		{
 			printf("\nRUNNING: Hold upright to balance.\n");
 			printf("                 SENSORS               |           ODOMETRY          |");
 			printf("\n");
@@ -230,12 +250,14 @@ void* printf_loop(void* ptr){
 
 			printf("\n");
 		}
-		else if(new_state==PAUSED && last_state!=PAUSED){
+		else if (new_state == PAUSED && last_state != PAUSED)
+		{
 			printf("\nPAUSED\n");
 		}
 		last_state = new_state;
-		
-		if(new_state == RUNNING){
+
+		if (new_state == RUNNING)
+		{
 			printf("\r");
 			//Add Print stattements here, do not follow with /n
 			pthread_mutex_lock(&state_mutex);
@@ -246,7 +268,7 @@ void* printf_loop(void* ptr){
 			pthread_mutex_unlock(&state_mutex);
 			fflush(stdout);
 		}
-		rc_nanosleep(1E9/PRINTF_HZ);
+		rc_nanosleep(1E9 / PRINTF_HZ);
 	}
 	return NULL;
-} 
+}
