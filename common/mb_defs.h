7987d4cc01b560413296d64cc66219e1ce5af777
Team 5 <zhsh@umich.edu>
2018-10-20 16:34:16 -0400

    Fix motor polarity after labelling robot

--- common/mb_defs.h
+++ common/mb_defs.h
@@ -18,8 +18,8 @@
 #define MDIR2_CHIP              1
 #define MDIR2_PIN               16  //gpio1.16  P9.15
 #define MOT_BRAKE_EN            20    // gpio0.20  P9.41
-#define MOT_1_POL               1    // polarity of motor 1
-#define MOT_2_POL               -1    // polarity of motor 2
+#define MOT_1_POL               -1    // polarity of motor 1
+#define MOT_2_POL               1    // polarity of motor 2
 #define ENC_1_POL               1    // polarity of encoder 1
 #define ENC_2_POL               -1    // polarity of encoder 2
 #define MOT_1_CS                3    // analog in of motor 1 current sense
