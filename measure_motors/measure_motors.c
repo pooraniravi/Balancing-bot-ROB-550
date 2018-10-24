835bf14daeafa1cb8b28cbe4e0d10952a77f5bec
Team 5 <zhsh@umich.edu>
2018-10-24 12:42:46 -0400

    Collect data to determine inertia of motor+wheel

--- measure_motors/measure_motors.c
+++ measure_motors/measure_motors.c
@@ -10,8 +10,8 @@
 #include <math.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/stat.h>	// for mkdir and chmod
-#include <sys/types.h>	// for mkdir and chmod
+#include <sys/stat.h>    // for mkdir and chmod
+#include <sys/types.h>    // for mkdir and chmod
 #include <rc/start_stop.h>
 #include <rc/cpu.h>
 #include <rc/encoder_eqep.h>
@@ -21,16 +21,23 @@
 #include "../common/mb_motor.h"
 #include "../common/mb_defs.h"
 #include "../common/mb_odometry.h"
+
 #define ROUNDS_TO_MEASURE 600
 
-FILE* f1;
+FILE *f1;
 
-int64_t us_now (void){
+int64_t us_now(void) {
     struct timeval tv;
-    gettimeofday (&tv, NULL);
+    gettimeofday(&tv, NULL);
     return (int64_t) tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
+/**
+ * Determine K and R through running in steady state (di/dt = 0)
+ * Remember to measure voltage across the motor with a multimeter
+ * @param motor
+ * @param duty
+ */
 void measureMotor(int motor, double duty) {
     double times[ROUNDS_TO_MEASURE];
     int ticks[ROUNDS_TO_MEASURE];
@@ -66,38 +73,81 @@ void measureMotor(int motor, double duty) {
     printf("Out of loop\n");
 }
 
+void measureInertia(int motor, double duty) {
+    double times[ROUNDS_TO_MEASURE];
+    int ticks[ROUNDS_TO_MEASURE];
+    double currents[ROUNDS_TO_MEASURE];
+
+    int seconds = 2;
+    // let motor run at no load
+    mb_motor_set(motor, duty);
+    // wait for it to enter steady state
+    rc_nanosleep(seconds * 1e9);
+
+    printf("Turning off motor\n");
+    resetEncoders();
+
+    // TODO should we break or not? take a look at current and then decide
+    if (mb_motor_brake(1) != 0) {
+        printf("Motor can't modify brake");
+        return;
+    };
+    // turn off motor
+    mb_motor_set(motor, 0);
+
+    int64_t startTime = us_now();
+
+    int64_t now = startTime;
+    int i = 0;
+    while (now - startTime < 2 * 1e6) {
+        now = us_now();
+        times[i] = now - startTime;
+        ticks[i] = rc_encoder_eqep_read(motor);
+        currents[i] = mb_motor_read_current(motor);
+
+        ++i;
+        rc_nanosleep(seconds * 1e7);
+    }
+
+    for (int j = 0; j < i; ++j) {
+        printf("%f, %d, %f\n", times[j], ticks[j], currents[j]);
+    }
+    mb_motor_set(motor, 0);
+    printf("Out of loop\n");
+}
+
 /*******************************************************************************
 * int main() 
 *
 *******************************************************************************/
-int main(){
-	
-	// make sure another instance isn't running
+int main() {
+
+    // make sure another instance isn't running
     // if return value is -3 then a background process is running with
     // higher privaledges and we couldn't kill it, in which case we should
     // not continue or there may be hardware conflicts. If it returned -4
     // then there was an invalid argument that needs to be fixed.
-    if(rc_kill_existing_process(2.0)<-2) return -1;
+    if (rc_kill_existing_process(2.0) < -2) return -1;
 
-	// start signal handler so we can exit cleanly
-    if(rc_enable_signal_handler()==-1){
-        fprintf(stderr,"ERROR: failed to start signal handler\n");
+    // start signal handler so we can exit cleanly
+    if (rc_enable_signal_handler() == -1) {
+        fprintf(stderr, "ERROR: failed to start signal handler\n");
         return -1;
     }
 
-	if(rc_cpu_set_governor(RC_GOV_PERFORMANCE)<0){
-        fprintf(stderr,"Failed to set governor to PERFORMANCE\n");
+    if (rc_cpu_set_governor(RC_GOV_PERFORMANCE) < 0) {
+        fprintf(stderr, "Failed to set governor to PERFORMANCE\n");
         return -1;
     }
 
-	// initialize enocders
-    if(rc_encoder_eqep_init()==-1){
-        fprintf(stderr,"ERROR: failed to initialize eqep encoders\n");
+    // initialize enocders
+    if (rc_encoder_eqep_init() == -1) {
+        fprintf(stderr, "ERROR: failed to initialize eqep encoders\n");
         return -1;
     }
 
     // initialize adc
-    if(rc_adc_init()==-1){
+    if (rc_adc_init() == -1) {
         fprintf(stderr, "ERROR: failed to initialize adc\n");
         return -1;
     }
@@ -108,19 +158,20 @@ int main(){
     }
 
     // make PID file to indicate your project is running
-	// due to the check made on the call to rc_kill_existing_process() above
-	// we can be fairly confident there is no PID file already and we can
-	// make our own safely.
-	rc_make_pid_file();
+    // due to the check made on the call to rc_kill_existing_process() above
+    // we can be fairly confident there is no PID file already and we can
+    // make our own safely.
+    rc_make_pid_file();
 
 
     rc_set_state(RUNNING);
 
     // try at 0.5 0.8 1.0
-    measureMotor(LEFT_MOTOR, 0.5);
+    //    measureMotor(LEFT_MOTOR, 0.5);
+    measureInertia(RIGHT_MOTOR, 1.0);
 
-	// exit cleanly
-	rc_encoder_eqep_cleanup();
-	rc_remove_pid_file();   // remove pid file LAST
-	return 0;
+    // exit cleanly
+    rc_encoder_eqep_cleanup();
+    rc_remove_pid_file();   // remove pid file LAST
+    return 0;
 }
