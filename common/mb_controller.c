4dab4cfefd7ca3d5d913fbbd4432e1c294e5a437
Johnson Zhong <johnsonzhong@hotmail.ca>
2018-11-08 15:19:57 -0500

    Use recommended controller rise times

--- common/mb_controller.c
+++ common/mb_controller.c
@@ -68,14 +68,14 @@ int mb_controller_load_config() {
     double kpPhi, kiPhi, kdPhi, maxTheta;
     fscanf(file, "%lf %lf %lf %lf", &kpPhi, &kiPhi, &kdPhi, &maxTheta);
 
-    if (rc_filter_pid(&thetaController, kpTheta, kiTheta, kdTheta, 4 * DT,
+    if (rc_filter_pid(&thetaController, kpTheta, kiTheta, kdTheta, 0.0278,
                       DT)) {
         fprintf(stderr,
                 "ERROR in rc_balance, failed to make theta controller\n");
         return -1;
     }
 
-    if (rc_filter_pid(&phiController, kpPhi, kiPhi, kdPhi, 4 * DT, DT)) {
+    if (rc_filter_pid(&phiController, kpPhi, kiPhi, kdPhi, 0.1944, DT)) {
         fprintf(stderr, "ERROR in rc_balance, failed to make phi controller\n");
         return -1;
     }
